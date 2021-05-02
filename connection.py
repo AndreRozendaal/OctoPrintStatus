61be958d84197f2f4c8b739e630f023629c115e8
andre rozendaal <andre.rozendaal@rabobank.nl>
2021-05-02 15:12:12 +0200

    return dict data structure with printer temperature

--- connection.py
+++ connection.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 class Connection:
     def __init__(self, url, api_key):
         self.url = url
@@ -10,6 +11,7 @@ class Connection:
             'Content-Type': 'application/json',
             'X-Api-Key': api_key
         }
+
     def __str__(self):
         return f"url: {self.url}\napi_key: {self.api_key}"
 
@@ -21,4 +23,19 @@ class Connection:
             raise SystemExit(e)
 
     def get_all(self):
-        return self.get("/printer")
+        data = {
+            "temp_bed": "N/A",
+            "temp_nozzle": "N/A",
+            "printer status": "N/A",
+            "elapsed_time": "N/A"
+        }
+        printer = self.get("/printer")
+        # printer = self.get("/job")
+
+        if printer.get("temperature"):
+            temperature = printer["temperature"]
+            if temperature.get("bed"):
+                data["temp_bed"] = printer["temperature"]["bed"]["actual"]
+            if temperature.get("tool0"):
+                data["temp_nozzle"] = printer["temperature"]["tool0"]["actual"]
+        return data
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -2,8 +2,6 @@ from connection import Connection
 from pytest import fixture
 from mock import Mock
 
-# test Connection.get_all with defined return values Connection.get {"sd":{"ready":"false"},"state":{"error":"","flags":{"cancelling":"false","closedOrError":"false","error":"false","finishing":"false","operational":"true","paused":"false","pausing":"false","printing":"false","ready":"true","resuming":"false","sdReady":"false"},"text":"Operational"},"temperature":{"bed":{"actual":20.78,"offset":0,"target":0.0},"tool0":{"actual":21.37,"offset":0,"target":0.0}}}
-AND  {"error":"Printer is not operational"}
-
-
-
+# test Connection.get_all with defined return values Connection.get :
+# {"sd":{"ready":"false"},"state":{"error":"","flags":{"cancelling":"false","closedOrError":"false","error":"false","finishing":"false","operational":"true","paused":"false","pausing":"false","printing":"false","ready":"true","resuming":"false","sdReady":"false"},"text":"Operational"},"temperature":{"bed":{"actual":20.78,"offset":0,"target":0.0},"tool0":{"actual":21.37,"offset":0,"target":0.0}}}
+# AND  {"error":"Printer is not operational"}
