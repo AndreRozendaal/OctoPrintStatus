17de6c5d8a9eb55e2328878777d378ae8a7f678f
andre rozendaal <andre.rozendaal@rabobank.nl>
2021-05-03 13:22:58 +0200

    change windows layout

--- gui.py
+++ gui.py
@@ -6,25 +6,43 @@ def gui(con):
         ['Edit', ['Configuration'], ],
         ['view', ['Status Bar'], ],
         ['Help', ['About Octoprint'], ], ]
-    WIN_W = 90
+    WIN_W = 50
     WIN_H = 25
     interval = 5000
 
-    layout = [[sg.Menu(menu_def)],
-              [sg.Text('Temp bed:'), sg.Text(size=(WIN_W, 1), key="bed")],
-              [sg.Text('Temp nozzle:'), sg.Text(size=(WIN_W, 1), key="nozzle")],
+    col_bed = [
+        [sg.T('bed', key="bed", size=(5,1))],
+    ]
 
-              [sg.Text(''), sg.Text(size=(WIN_W, 1), key="text2")],
-              [sg.StatusBar(text=f'ok', size=(WIN_W, 1), pad=(0, 0), text_color='black',
-                            background_color='white', relief=sg.RELIEF_FLAT, justification='left', visible=False,
-                            key='status_bar')]
-              ]
-    status_bar_switch = True
-    window = sg.Window(f"Octoprint", layout, keep_on_top=False, resizable=True, finalize=True)
+    col_nozzle = [
+        [sg.T('nozzle', key="nozzle", size=(5,1))],
+    ]
+
+    col_completion = [
+        [sg.T('N/A', key="completion", size=(5,1))],
+    ]
+
+    col_timeleft = [
+        [sg.T('time left', key="printTimeLeft", size=(5,1))],
+    ]
+    layout = [[sg.Menu(menu_def)],
+            [sg.Frame('', col_nozzle),
+               sg.Frame('', col_bed)],
+              [sg.Frame('', col_completion),
+               sg.Frame('', col_timeleft)],
+              [sg.StatusBar(text=f'', pad=(5, 5), size=(100,1),  text_color='black',
+                            background_color='white', relief=sg.RELIEF_FLAT, justification='left', visible=True,
+                            key='status_bar')]]
+
+    status_bar_switch = False
+    window = sg.Window(f"Octoprint", layout, keep_on_top=False, resizable=True, finalize=True, size=(162,110))
     while True:
         result = con.get_all()
         window['bed'].update(result["temp_bed"])
         window['nozzle'].update(result["temp_nozzle"])
+        window['printTimeLeft'].update(result["elapsed_time"])
+        window['status_bar'].update(result["printer_status"])
+        print(result["printer_status"])
         event, values = window.read(timeout=interval)
 
         if event in (sg.WIN_CLOSED, 'Exit'):
